bef0795fbebf1426f9cccd45321e17ea3cd30f65
Mr. Akash Rathod <103313132+ak-akashhack@users.noreply.github.com>
2022-05-04 21:51:26 +0530

    Update java01.java

--- src/java01.java
+++ src/java01.java
@@ -1,7 +1,14 @@
 public class java01 {
     public static void main(String[] args) {
         System.out.println("this is the java program : ");
-        System.out.println("this is the java program : ");
-        System.out.println("this is the java program : ");
+        System.out.println("hello akash");
+        System.out.println("hello akash");
+        System.out.println("hello akash");
+        System.out.println("hello akash");
+        System.out.println("hello akash");
+        System.out.println("hello akash");
+        System.out.println("hello akash");
+        
+    
     }
 }
