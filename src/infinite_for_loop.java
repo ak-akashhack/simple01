1449ea894f9ee07033af74ce74e403d79dc526b6
Akash Rajendra Rathod <ak.hackwithakash@gmail.com>
2022-05-06 06:36:45 +0530

    this is the big commits

---
+++ src/infinite_for_loop.java
@@ -0,0 +1,12 @@
+
+// Write the infinite for loops :
+
+public class infinite_for_loop {
+    public static void main(String[] args) {
+
+        for (;;){
+            System.out.println("hallo akash : ");
+        }
+
+    }
+}
